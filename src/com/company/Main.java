84f711f4a49a1c9a30af2f8e42ac8e8ae044c303
Tagir248 <tagir248@mail.ru>
2021-07-13 14:47:59 +0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+import javax.sql.rowset.serial.SQLOutputImpl;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+    }
+}
